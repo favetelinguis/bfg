7ad98163c474f7ecc831e4eaa6b249f40a8d3959
Henrik Larsson <henke@Henriks-MBP.lan>
2016-03-13 20:27:41 +0100

    Commented out test for now

--- src/bfg/core.clj
+++ src/bfg/core.clj
@@ -1,6 +1,9 @@
 (ns bfg.core
   (:require
    [bfg.ws :as ws]
+   [ring.middleware.resource :refer (wrap-resource)]
+   [ring.middleware.content-type :refer (wrap-content-type)]
+   [ring.middleware.not-modified :refer (wrap-not-modified)])
    [immuconf.config :as immuconf]
    [immutant.web             :as web]
    [immutant.web.async       :as async]
@@ -13,6 +16,9 @@
 (defn -main [& {:as args}]
   (web/run
     (-> ws/routes
+        (wrap-resource "public")
+        (wrap-content-type)
+        (wrap-not-modified)
         (web-middleware/wrap-session {:timeout 20})
         ;; wrap the handler with websocket support
         ;; websocket requests will go to the callbacks, ring requests to the handler
